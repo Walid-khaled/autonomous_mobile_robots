8c197ca692bbb4b75283f8ad44366ec90ad8300c
Walid-khaled <w.shaker@innopolis.university>
2023-02-06 17:51:07 +0300

    add package: hagen_gazebo

--- run1.sh
+++ run1.sh
@@ -3,6 +3,6 @@ source /opt/ros/humble/setup.bash
 . /usr/share/gazebo/setup.sh
 . ~/ros2_ws/install/setup.bash
 echo "Launching application, please wait!"
-ros2 launch hagen_gazebo hagen.launch.py 
+ros2 launch hagen_gazebo1 hagen.launch.py world:=hagen_city.world
 
 
