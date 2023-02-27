b11bb25fcc2944c5abb55e86f8034eaa05a2e930
Walid Shaker <w.khaled@nu.edu.eg>
2023-02-27 23:07:06 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@ source /opt/ros/humble/setup.bash
 . ~/ros2_ws/install/setup.bash
 echo "Launching application, please wait!"
 ros2 launch hagen_gazebo hagen.launch.py 
+# ros2 launch hagen_gazebo1 hagen.launch.py world:=hagen_city.world
 
 
