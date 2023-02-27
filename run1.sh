e0fa5dd4065cbeb215d53537518fe52435fc807c
Walid Shaker <w.khaled@nu.edu.eg>
2023-02-27 23:07:15 +0300

    Delete run1.sh

--- run1.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd ~/ros2_ws/src/autonomous_mobile_robots && code .
-source /opt/ros/humble/setup.bash
-. /usr/share/gazebo/setup.sh
-. ~/ros2_ws/install/setup.bash
-echo "Launching application, please wait!"
-ros2 launch hagen_gazebo1 hagen.launch.py world:=hagen_city.world
-
-
